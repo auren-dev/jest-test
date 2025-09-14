c2536510ff6e4c4cdc6967da22d79bcc7fb24c0e
hyun <iamhyunji_kim@icloud.com>
2025-09-15 08:10:12 +0900

    add coverage badge

--- jest.config.js
+++ jest.config.js
@@ -36,10 +36,11 @@ const config = {
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: [
-    "json",
-  //   "text",
+    // "json",
+    //   "text",
     "lcov",
-  //   "clover"
+    //   "clover"
+    "json-summary"
   ],
 
   // An object that configures minimum threshold enforcement for coverage results
@@ -79,15 +80,15 @@ const config = {
   // An array of file extensions your modules use
   moduleFileExtensions: [
     "js",
-  //   "mjs",
-  //   "cjs",
-  //   "jsx",
+    //   "mjs",
+    //   "cjs",
+    //   "jsx",
     "ts",
-  //   "mts",
-  //   "cts",
-  //   "tsx",
-  //   "json",
-  //   "node"
+    //   "mts",
+    //   "cts",
+    //   "tsx",
+    //   "json",
+    //   "node"
   ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
