0f3b0e1da45e088ba69779e9c2b1c4c7d0c9a158
hyun <iamhyunji_kim@icloud.com>
2025-09-15 09:57:25 +0900

    add counter test placeholder

--- test/couter.test.ts
+++ test/couter.test.ts
@@ -1,4 +1,8 @@
 import { describe, beforeEach, afterEach, test, expect } from '@jest/globals'
 import Counter from '../src/couter'
 
+test('placeholder', () => {
+    expect(true).toBe(true)
+})
+
 // ❗️TODO:: 요구사항을 만족하도록 코드를 작성하세요
